0a3899783becb0295128f7b0e9b2666c24d7853c
Glenn <glennwiz@gmail.com>
2023-10-27 18:42:38 +0200

    Enhance Rule30Simulation with Throttling and Color Gradients

--- Program.cs
+++ Program.cs
@@ -29,18 +29,28 @@ namespace Rule30Simulation
             window.KeyPressed += OnKeyPressed;
             window.KeyReleased += OnKeyReleased;
 
+            Clock clock = new Clock();
+
             while (window.IsOpen)
             {
                 window.DispatchEvents();
 
                 if (isDownKeyPressed)
                 {
-                    ScrollGridUp();
+                    if (clock.ElapsedTime.AsMilliseconds() > 200)
+                    {
+                        ScrollGridUp();
+                        clock.Restart();
+                    }
                 }
 
                 if (isUpKeyPressed)
                 {
-                    ScrollGridDown();
+                    if (clock.ElapsedTime.AsMilliseconds() > 200)
+                    {
+                        ScrollGridDown();
+                        clock.Restart();
+                    }
                 }
 
                 UpdateGrid();
@@ -78,52 +88,62 @@ namespace Rule30Simulation
 
         static bool ApplyRule30(bool left, bool center, bool right)
         {
-            return (left && !center && !right) || (!left && center && right) || (!left && center && !right) || (!left && !center && right);
+            if (left && !center && !right) return true;  // \\ 100 in haed like bom not not
+            if (!left && center && right) return true;   // \\ 011 in head like not bom bom
+            if (!left && center && !right) return true;  // \\ 010 in head like not bom not
+            if (!left && !center && right) return true;  // \\ 001 in head like not not bom
+
+            return false;
         }
 
         static void ScrollGridUp()
         {
-            for (uint y = 30; y < generations; y++)
+            for (int y = (int)(generations - 1); y >= 1; y--)
             {
                 for (uint x = 0; x < gridSize; x++)
                 {
-                    grid[x, y - 30] = grid[x, y];
+                    grid[x, y] = grid[x, y - 1];
                 }
             }
 
-            // Clear the bottom rows
-            for (uint y = generations - 30; y < generations; y++)
+            // Clear the top row
+            for (uint x = 0; x < gridSize; x++)
             {
-                for (uint x = 0; x < gridSize; x++)
-                {
-                    grid[x, y] = false;
-                }
+                grid[x, 0] = false;
             }
         }
 
         static void ScrollGridDown()
         {
-            for (uint y = generations - 31; y > 0; y--)
+            for (uint y = generations - 1; y > 0; y--)
             {
                 for (uint x = 0; x < gridSize; x++)
                 {
-                    grid[x, y] = grid[x, y - 30];
+                    grid[x, y] = grid[x, y - 1];
                 }
             }
 
-            // Clear the top rows
-            for (uint y = 0; y < 30; y++)
+            // Clear the bottom row
+            for (uint x = 0; x < gridSize; x++)
             {
-                for (uint x = 0; x < gridSize; x++)
-                {
-                    grid[x, y] = false;
-                }
+                grid[x, 0] = false;
             }
         }
-        
+
+        // Linearly interpolate between two colors based on a factor between 0 and 1
+        static Color LerpColor(Color a, Color b, float factor)
+        {
+            return new Color(
+                (byte)(a.R + factor * (b.R - a.R)),
+                (byte)(a.G + factor * (b.G - a.G)),
+                (byte)(a.B + factor * (b.B - a.B))
+            );
+        }
+
         static void DrawGrid()
         {
             window.Clear(Color.Black);
+            uint segment = generations / 4;
 
             for (uint y = 0; y < generations; y++)
             {
@@ -131,16 +151,35 @@ namespace Rule30Simulation
                 {
                     if (grid[x, y])
                     {
+                        Color color;
+                        if (y < segment) // White to Red
+                        {
+                            color = LerpColor(Color.White, Color.Red, (float)y / segment);
+                        }
+                        else if (y < 2 * segment) // Red to Blue
+                        {
+                            color = LerpColor(Color.Red, Color.Blue, (float)(y - segment) / segment);
+                        }
+                        else if (y < 3 * segment) // Blue to Green
+                        {
+                            color = LerpColor(Color.Blue, Color.Green, (float)(y - 2 * segment) / segment);
+                        }
+                        else // Green to White
+                        {
+                            color = LerpColor(Color.Green, Color.White, (float)(y - 3 * segment) / segment);
+                        }
+
                         RectangleShape cell = new RectangleShape(new Vector2f(cellSize, cellSize))
                         {
                             Position = new Vector2f(x * cellSize, y * cellSize),
-                            FillColor = Color.White
+                            FillColor = color
                         };
                         window.Draw(cell);
                     }
                 }
             }
         }
+
 
         static void OnKeyPressed(object sender, KeyEventArgs e)
         {
